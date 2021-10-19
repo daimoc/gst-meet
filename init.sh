bcd50129303badfb0a940e6a227665d3d62c5a4f
daimoc <dfetis@gmail.com>
2021-10-19 23:26:18 +0200

    Add sudo command in install init script

--- init.sh
+++ init.sh
@@ -2,9 +2,10 @@ TEMP_DEB="$(mktemp)"
 
 
 
-sh build-gst.sh
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source $HOME/.cargo/env
+sudo sh /vagrant/build-gst.sh
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rust.#!/bin/sh
+sudo sh rust.sh -y
+sudo source $HOME/.cargo/env
 
 cd /vagrant
-cargo build --release -p gst-meet
+sudo cargo build --release -p gst-meet
