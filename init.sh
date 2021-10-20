0b2e1ef3d6d593b0901d12976be7ce7a50124375
daimoc <dfetis@gmail.com>
2021-10-20 23:32:45 +0200

    Fix init.sh script to install cargo

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@ TEMP_DEB="$(mktemp)"
 
 
 
-sudo sh /vagrant/build-gst.sh
-sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rust.#!/bin/sh
+#sudo sh /vagrant/build-gst.sh
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rust.sh
 sudo sh rust.sh -y
 sudo source $HOME/.cargo/env
 
